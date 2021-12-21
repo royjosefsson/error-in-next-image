449bf046bae4e1818f3bfaddc5eac14900a864e1
Roy Josefsson <roy.josefsson@gmail.com>
2021-12-21 16:27:12 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
+const withPlugins = require('next-compose-plugins');
+const withFonts = require('next-fonts');
+const withImages = require('next-images');
+
+module.exports = withPlugins([withFonts, withImages], {
   reactStrictMode: true,
-}
+  compress: true,
+  productionBrowserSourceMaps: true,
+  images: {
+    domains: ["media.meds.se"],
+  }
+});
