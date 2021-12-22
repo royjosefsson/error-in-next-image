f4ab63bc7fadcbd33f1482b4c62bf7a994e693e0
Roy Josefsson <roy.josefsson@gmail.com>
2021-12-22 10:13:01 +0100

    optimized vs unoptimized

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,27 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className={styles.container}>
-    <h1>Image from Meds</h1>
-      <Image
-        src="https://media.meds.se/meds/images/maxx-27397425-g-2019-11-13-111615299/455/360/0/dr-hauschka-lip-balm-4-5ml.jpg"
-        width={100}
-        height={100}
-        alt='Image from meds.se'
-      />
+      <h2>Optimized image from Meds</h2>
+      <div>
+        <Image
+          src="https://media.meds.se/meds/images/maxx-27397425-g-2019-11-13-111615299/455/360/0/dr-hauschka-lip-balm-4-5ml.jpg"
+          width={100}
+          height={100}
+          alt='Image from meds.se'
+          unoptimized={false}
+        />
+      </div>
+
+      <h2>Unoptimized image from Meds</h2>
+      <div>
+        <Image
+          src="https://media.meds.se/meds/images/maxx-27397425-g-2019-11-13-111615299/455/360/0/dr-hauschka-lip-balm-4-5ml.jpg"
+          width={100}
+          height={100}
+          alt='Image from meds.se'
+          unoptimized={true}
+        />
+      </div>
     </div>
   )
 }
